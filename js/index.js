e86ad457b9d3acaff363797d8632930d5111a6ad
Maksim Perevalov <febramiks@gmail.com>
2024-02-08 20:21:39 +0300

    Исправил курсор

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 if (window.innerWidth > 498) {
     swiper.destroy()
 }  
-const element = document.querySelector('.swiper_btn-el');
+const element = document.querySelector('.swiper-btn-el');
 const elements = document.querySelector('.swiper');
-const text = document.querySelector('.swiper_btn-text');
-const icon = document.querySelector('.swiper_btn-img');
+const text = document.querySelector('.swiper-btn-text');
+const icon = document.querySelector('.swiper-btn-img');
 
 
 element.addEventListener('click', () => {
   if (text.textContent === 'Показать всё') {
     text.textContent = 'Скрыть';
     elements.classList.remove("swiper--notAll");
-    icon.classList.add('swiper_btn-img--transform');
+    icon.classList.add('swiper-btn-img--transform');
 
   } else {
     text.textContent = 'Показать всё';
     elements.classList.add("swiper--notAll");
-    icon.classList.remove('swiper_btn-img--transform')
+    icon.classList.remove('swiper-btn-img--transform')
   }
 });
 
